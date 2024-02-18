399d6d30490355bef99d73d4978a98ad33120e14
iyozou1470 <naotaka.i70@gmail.com>
2024-02-18 22:55:33 +0900

    会員側の認証を実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('kana'); // 'kana' カラムのデータ型を追加
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('birthday')->nullable();
+            $table->string('occupation')->nullable();
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); // 'created_at' と 'updated_at' カラムの追加方法を修正
+
         });
     }
 
