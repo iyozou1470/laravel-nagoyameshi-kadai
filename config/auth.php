a2d5daeab18f512fb668bfbccc1f11bed2b11fb7
iyozou1470 <naotaka.i70@gmail.com>
2024-02-26 15:04:06 +0900

    管理者側の認証を実装

--- config/auth.php
+++ config/auth.php
@@ -17,6 +17,7 @@ return [
         'guard' => 'web',
         'passwords' => 'users',
     ],
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -40,7 +41,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+    'admin' => [
+        'driver' => 'session',
+        'provider' => 'admins',
     ],
+],
+
 
     /*
     |--------------------------------------------------------------------------
@@ -64,6 +70,10 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
+         'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+         ],
 
         // 'users' => [
         //     'driver' => 'database',
