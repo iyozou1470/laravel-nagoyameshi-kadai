22c64891bd88bddcd860ba391b443e657d423aaa
iyozou1470 <naotaka.i70@gmail.com>
2024-07-15 00:16:21 +0900

    Add CategorySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             AdminSeeder::class,
             Admin2Seeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
